9aa0d6c0999af9b9565dda58741dcbd980059efc
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-10-08 21:44:36 +0530

    hero dynamic type

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -4,3 +4,17 @@ const menu = document.querySelector('.menu');
 menuIcon.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+
+var index = 0; 
+var text = 'Hello Geeks of GeeksforGeeks'; 
+var speed = 50; 
+
+function textEffect() { 
+	if (index < text.length) { 
+		document.getElementById("effect") 
+				.innerHTML += text.charAt(index); 
+		index++; 
+		setTimeout(textEffect, speed); 
+	} 
+}
